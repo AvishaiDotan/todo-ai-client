644351315b8bbebbda6edefa87b8940f468780fc
Y-Moshe <moshe.nehemiah@gmail.com>
2023-06-12 16:00:23 +0300

    Add baseURL based on the development environments

--- src/App/Services/http.service.ts
+++ src/App/Services/http.service.ts
@@ -1,7 +1,9 @@
 import Axios from 'axios'
 
 export const axios = Axios.create()
-const baseURL = 'http://todo-ai-server.us-east-1.elasticbeanstalk.com/api'
+const baseURL = import.meta.env.DEV
+  ? 'http://localhost:5065/api'
+  : 'http://todo-ai-server.us-east-1.elasticbeanstalk.com/api'
 
 export const httpService = {
   get<T>(endpoint: string, data = null) {
