b4ca2d7cdd95fcbe6727e6152f81c59ad44cc0a9
Y-Moshe <moshe.nehemiah@gmail.com>
2023-06-18 18:43:34 +0300

    Using tailwind and app theme colors

--- src/App/Pages/AuthPage.tsx
+++ src/App/Pages/AuthPage.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, FormEvent, useMemo, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { ImSpinner2 } from 'react-icons/im'
 
 import { loginUser, registerUser } from '@/Store/Actions/account.actions'
@@ -40,8 +40,8 @@ export default function AuthPage(props: IAuthPageProps) {
   const submitBtnClasses = useMemo(
     () =>
       props.isLoginPage
-        ? `bg-sky-600 hover:text-sky-600  hover:border-sky-600`
-        : `bg-green-600 hover:text-green-600  hover:border-green-600`,
+        ? `bg-secondary hover:text-secondary  hover:border-secondary`
+        : `bg-primary hover:text-primary  hover:border-primary`,
     [props.isLoginPage]
   )
 
@@ -79,10 +79,15 @@ export default function AuthPage(props: IAuthPageProps) {
 
   return (
     <div className='auth-page'>
-      <h1>{props.isLoginPage ? 'Log-in' : 'Sign-up'}</h1>
       <form
         onSubmit={handleSubmit}
         className='w-full md:w-1/2 xl:w-1/3 2xl:w-1/4 flex flex-col gap-3'>
+        <h1
+          className={`p-2 text-white text-center text-2xl w-full ${
+            props.isLoginPage ? 'bg-secondary' : 'bg-primary'
+          }`}>
+          {props.isLoginPage ? 'Log-in' : 'Sign-up'}
+        </h1>
         {inputs.map((inp) => (
           <React.Fragment key={inp.name}>
             <label htmlFor={`${inp.name}-input`}>{inp.label}</label>
@@ -92,10 +97,26 @@ export default function AuthPage(props: IAuthPageProps) {
               id={`${inp.name}-input`}
               value={formInputs[inp.name]}
               onChange={handleChange}
-              className='w-full px-5 py-2 transition focus:outline-none focus:border-sky-600 border border-2 border-emerald-900 rounded hover:border-sky-500'
+              className='w-full px-5 py-2 transition focus:outline-none focus:border-secondary border border-2 border-emerald-900 rounded hover:border-sky-500'
             />
           </React.Fragment>
         ))}
+
+        {props.isLoginPage ? (
+          <p className='text-gray-500'>
+            Don't you have an account?{' '}
+            <Link to={'/signup'} className='text-sky-500'>
+              Signup
+            </Link>
+          </p>
+        ) : (
+          <p className='text-gray-500'>
+            Already have an account?{' '}
+            <Link to={'/login'} className='text-sky-500'>
+              Login
+            </Link>
+          </p>
+        )}
 
         <p
           className={`transition duration-500 px-2 py-3 bg-red-300 border border-red-400 text-red-600 rounded ${
