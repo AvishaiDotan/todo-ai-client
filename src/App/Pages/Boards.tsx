d1ab74982f274822fe3b3d194c9a9a26d98ef21f
Avishai Dotan <108017307+AvishaiDotan@users.noreply.github.com>
2023-06-22 01:04:50 +0300

    add content editable and fix bug

--- src/App/Pages/Boards.tsx
+++ src/App/Pages/Boards.tsx
@@ -13,80 +13,81 @@ import TableBody from './TableComponents/TableBody'
 import TodoAiLoader from '@/Components/MiniComponents/TodoAiLoader'
 
 type ComponentParams = {
-  boardId?: string
-  todoId?: string
+	boardId?: string
+	todoId?: string
 }
 
 interface IBoardsProps {
-  title: string
-  dataToRenderType: DataToRenderTypeEnum
+	title: string
+	dataToRenderType: DataToRenderTypeEnum
 }
 
 export default function Boards({ title, dataToRenderType }: IBoardsProps) {
-  const [dataToRender, setDataToRender] = useState<DataToRender>([])
-  const params = useParams<ComponentParams>()
+	const [dataToRender, setDataToRender] = useState<DataToRender>([])
+	const [isLoading, setIsLoading] = useState<boolean>(false)
+	const params = useParams<ComponentParams>()
+	
+	useEffect(() => {
+		loadContent()
+	}, [params])
 
-  useEffect(() => {
-    loadContent()
-  }, [params])
+	const loadContent = async () => {
+		let content: DataToRender = await boardService.getBoards()
 
-  const loadContent = async () => {
-    let content: DataToRender = await boardService.getBoards()
+		const { boardId, todoId } = params
+		
+		if (todoId && boardId) {
+			const board = content.find((b) => b.id === +boardId)
+			const todo = board?.todos.find((t) => t.id === +todoId)
+			todo && setDataToRender(todo.subTasks)
+		} else if (
+			!boardId &&
+			!todoId &&
+			dataToRenderType == DataToRenderTypeEnum.board
+		) {
+			setDataToRender(content)
+		} else if (boardId && !todoId) {
+			const board = content.find((b) => b.id === +boardId)
+			board && setDataToRender(board.todos)
+		}
+	}
 
-    const { boardId, todoId } = params
+	return (
+		<section className='boards-page'>
+			<TableHeaderTitle headerStr={title} />
+			<section className='table-wrapper'>
+				<section className='table'>
+					<TableHeaders />
+					<div style={{ height: 'calc(100% - 60px)' }}>
+						{dataToRender.length ? (
+							<TableBody
+								dataToRender={dataToRender}
+								dataToRenderType={dataToRenderType}
+							/>
+						) : (
+							<TodoAiLoader />
+						)}
+					</div>
+				</section>
+			</section>
 
-    if (todoId && boardId) {
-      const board = content.find((b) => b.id === +boardId)
-      const todo = board?.todos.find((t) => t.id === +todoId)
-      todo && setDataToRender(todo.subTasks)
-    } else if (
-      !boardId &&
-      !todoId &&
-      dataToRenderType == DataToRenderTypeEnum.board
-    ) {
-      setDataToRender(content)
-    } else if (boardId && !todoId) {
-      const board = content.find((b) => b.id === +boardId)
-      board && setDataToRender(board.todos)
-    }
-  }
+			<section className='actions-wrapper'>
+				<div className='apply-status-to-all-wrapper'>
+					<CameraWrapper isFromHomePage={false}>
+						<div className='apply-status-to-all-container'>
+							<h4 className='third-font-family'>Status:</h4>
+							<TodoAICheckbox />
+							<div className='checkbox-arrow-wrapper'>
+								<img src={arrow} />
+							</div>
+						</div>
+					</CameraWrapper>
+				</div>
 
-  return (
-    <section className='boards-page'>
-      <TableHeaderTitle headerStr={title} />
-      <section className='table-wrapper'>
-        <section className='table'>
-          <TableHeaders />
-          <div style={{ height: 'calc(100% - 60px)' }}>
-            {dataToRender.length ? (
-              <TableBody
-                dataToRender={dataToRender}
-                dataToRenderType={dataToRenderType}
-              />
-            ) : (
-              <TodoAiLoader />
-            )}
-          </div>
-        </section>
-      </section>
+				<div className='description'></div>
 
-      <section className='actions-wrapper'>
-        <div className='apply-status-to-all-wrapper'>
-          <CameraWrapper isFromHomePage={false}>
-            <div className='apply-status-to-all-container'>
-              <h4 className='third-font-family'>Status:</h4>
-              <TodoAICheckbox />
-              <div className='checkbox-arrow-wrapper'>
-                <img src={arrow} />
-              </div>
-            </div>
-          </CameraWrapper>
-        </div>
-
-        <div className='description'></div>
-
-        {/* {(props.isBoards) && <div>Back to Boards Page</div>} */}
-      </section>
-    </section>
-  )
+				{/* {(props.isBoards) && <div>Back to Boards Page</div>} */}
+			</section>
+		</section>
+	)
 }
