89e22a288a3a2f3c388f00c9e181f6e04e61275d
Y-Moshe <moshe.nehemiah@gmail.com>
2023-06-21 19:03:43 +0300

    Add input placeholders and styles

--- src/App/Pages/AuthPage.tsx
+++ src/App/Pages/AuthPage.tsx
@@ -12,6 +12,7 @@ import { loginUser, registerUser } from '@/Store/Actions/account.actions'
 import { useAppDispatch } from '@/Store'
 import { gBoards } from '@/Services/board.service'
 import { createManyBoards } from '@/Store/Actions/boards.actions'
+import { IRegisterPayload } from '@/Types'
 
 interface IAuthPageProps {
   isLoginPage: boolean
@@ -41,19 +42,37 @@ export default function AuthPage(props: IAuthPageProps) {
   const inputs = useMemo(
     () =>
       [
-        { type: 'text', name: 'firstName', label: 'First Name' },
-        { type: 'text', name: 'lastName', label: 'Last Name' },
-        { type: 'email', name: 'email', label: 'E-Mail' },
-        { type: 'password', name: 'password', label: 'Password', minLength: 6 },
+        {
+          type: 'text',
+          name: 'firstName',
+          label: 'First Name',
+          placeholder: 'Type your first name',
+        },
+        {
+          type: 'text',
+          name: 'lastName',
+          label: 'Last Name',
+          placeholder: 'Type your last name',
+        },
+        {
+          type: 'email',
+          name: 'email',
+          label: 'E-Mail',
+          placeholder: 'Type your email address',
+        },
+        {
+          type: 'password',
+          name: 'password',
+          label: 'Password',
+          minLength: 6,
+          placeholder: 'Type your password',
+        },
       ].slice(props.isLoginPage ? 2 : 0),
     [props.isLoginPage]
   )
 
   const submitBtnClasses = useMemo(
-    () =>
-      props.isLoginPage
-        ? `bg-secondary hover:text-secondary  hover:border-secondary`
-        : `bg-primary hover:text-primary  hover:border-primary`,
+    () => (props.isLoginPage ? 'border-secondary login' : 'border-primary'),
     [props.isLoginPage]
   )
 
@@ -97,7 +116,7 @@ export default function AuthPage(props: IAuthPageProps) {
         onSubmit={handleSubmit}
         className='w-full md:w-1/2 xl:w-1/3 2xl:w-1/4 flex flex-col gap-3'>
         <h1
-          className={`p-2 text-white text-center text-2xl w-full ${
+          className={`p-2 text-white font-bold text-center text-2xl w-full ${
             props.isLoginPage ? 'bg-secondary' : 'bg-primary'
           }`}>
           {props.isLoginPage ? 'Log-in' : 'Sign-up'}
@@ -111,7 +130,7 @@ export default function AuthPage(props: IAuthPageProps) {
               id={`${inp.name}-input`}
               value={formInputs[inp.name]}
               onChange={handleChange}
-              className='w-full px-5 py-2 transition focus:outline-none focus:border-secondary border border-2 border-emerald-900 rounded hover:border-sky-500'
+              className='w-full px-5 py-2 transition focus:outline-none border-b focus:border-sky-500 rounded'
             />
           </React.Fragment>
         ))}
@@ -142,7 +161,7 @@ export default function AuthPage(props: IAuthPageProps) {
         <button
           type='submit'
           disabled={isSubmitting}
-          className={`shadow rounded-full text-white px-3 py-2 text-lg transition duration-300 hover:bg-white border border-2 ${submitBtnClasses}`}>
+          className={`submit-auth-btn rounded-full px-3 py-2 text-lg border border-2 ${submitBtnClasses}`}>
           {submitText}
         </button>
       </form>
