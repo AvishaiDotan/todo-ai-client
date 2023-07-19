8e7d539e7a760e700e4a5ab460728182b8a36655
Y-Moshe <moshe.nehemiah@gmail.com>
2023-07-19 23:51:35 +0300

    Important commit related to sortablejs and drap&drop

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import { Provider } from 'react-redux'
 import { router } from './App/Router'
 import { store } from './App/Store'
 
+// Disable auto freezer to enable sortablejs works well
+import { setAutoFreeze } from 'immer'
+setAutoFreeze(false)
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
     <RouterProvider router={router} />
